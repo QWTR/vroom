ab37ec6377cec5d66de27b6697010545515f69de
idea3oliwierp <oliwier.peciak@idea3.pl>
2026-04-16 18:35:37 +0200

    dsf

--- constants/mapConfig.ts
+++ constants/mapConfig.ts
@@ -2,7 +2,7 @@ const _token = process.env.EXPO_PUBLIC_MAPBOX_TOKEN;
 if (!_token && __DEV__) {
   console.warn('[mapConfig] EXPO_PUBLIC_MAPBOX_TOKEN is not set. Set it in your .env file.');
 }
-export const MAPBOX_TOKEN           = _token ?? '';
+export const MAPBOX_TOKEN           = 'pk.eyJ1IjoicDFrM3kiLCJhIjoiY21vMWx4Ym14MDZzdzJyc2VmOW1jNmNuaCJ9.hvV-mM6a1--RhnJqlMkojg';
 export const MAPBOX_STYLE_DARK      = 'mapbox://styles/mapbox/dark-v11';
 export const MAPBOX_STYLE_LIGHT     = 'mapbox://styles/mapbox/light-v11';
 export const MAPBOX_STYLE_SATELLITE = 'mapbox://styles/mapbox/satellite-streets-v12';
