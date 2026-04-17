744cf7c048212fb19ee0f6e84d68ab213292e8c0
idea3oliwierp <oliwier.peciak@idea3.pl>
2026-04-17 02:59:38 +0200

    enable new architecture and fix mapbox

--- app.config.js
+++ app.config.js
@@ -1,20 +1,19 @@
 const base = require('./app.json');
 
-/** @type {import('@expo/config').ExpoConfig} */
 module.exports = ({ config }) => {
-  // Używamy Tajnego Tokenu do pobierania (zmienna bez EXPO_PUBLIC)
-  const downloadToken = process.env.RNMAPBOX_MAPS_DOWNLOAD_TOKEN || '';
+  const downloadToken = process.env.RNMAPBOX_MAPS_DOWNLOAD_TOKEN || base.expo.plugins.find(p => Array.isArray(p) && p[0] === '@rnmapbox/maps')?.[1]?.RNMAPBOX_MAPS_DOWNLOAD_TOKEN;
 
   return {
     ...base.expo,
     ...config,
+    newArchEnabled: true, // Wymuszenie tutaj
     plugins: (base.expo.plugins ?? []).map((plugin) => {
       if (Array.isArray(plugin) && plugin[0] === '@rnmapbox/maps') {
         return [
           '@rnmapbox/maps',
           {
+            ...plugin[1],
             RNMapboxMapsImpl: 'mapbox',
-            // POPRAWIONA NAZWA KLUCZA I ZMIENNA
             RNMAPBOX_MAPS_DOWNLOAD_TOKEN: downloadToken,
           },
         ];
