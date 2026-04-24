db1f95539e63808b970c3dad7ee1d930e0d279a3
idea3oliwierp <oliwier.peciak@idea3.pl>
2026-04-24 02:03:15 +0200

    ads

--- components/ads/AdNativePost.tsx
+++ components/ads/AdNativePost.tsx
@@ -14,7 +14,7 @@ import {
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import { useTheme } from '../../contexts/ThemeContext';
 
-const NATIVE_ID = TestIds.NATIVE
+const NATIVE_ID = 'ca-app-pub-1660420496578702/9815615187'
 // const NATIVE_ID = __DEV__
 //   ? TestIds.NATIVE
 //   : 'ca-app-pub-1660420496578702/9615191240';
